0a250ed10ae0c259494d627e8a014f99ede1eef0
Andreas Nordenhof <andreasnordenhof@gmail.com>
2022-10-31 02:49:38 +0100

    testing stuff

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VueRouter from "vue-router"
-import vueConfig from 'vue.config'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 Vue.use(VueRouter)
-
-import VueAxios from "vue-axios"
-import axios from "axios"
-Vue.use(VueAxios)
-
-createApp(App).mount('#app')
