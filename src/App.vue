ef05a586bbdc5f55ff7b3e8aee15d0dc2b8e1145
Andreas Nordenhof <andreasnordenhof@gmail.com>
2022-10-31 18:02:36 +0100

    problems solved for now

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <nav class="header">
+        <div class="header__logo">
+            <!--some logo -->
+        </div>
+        <div class="header__nav">
+            <router-link class="header__nav__link btn btn-primary" to="/">Home</router-link>
+            <router-link class="header__nav__link" to="/about">About</router-link>
+        </div>
+    </nav>
+    <router-view/>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import {createApp} from 'vue'
+import App from './App.vue'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap"
 
-const app = Vue.createApp({
-    router,
-})
-app.use(VueRouter)
-app.mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
